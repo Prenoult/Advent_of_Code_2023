f27d43c91038a61b40b30b832354007789b67ce7
Paul <prenoult@me.com>
2023-12-03 15:34:54 +0100

    feat: Add Day3 part2

--- Day 3/index.js
+++ Day 3/index.js
@@ -97,4 +97,64 @@ const res = everyNumbersAdjacentToSpecialChar.reduce((acc, numberAdjacent) => {
 }, 0);
 fs.writeFileSync('./Day 3/output.txt', res.toString(), 'utf8');
 
+// part #2
+const isStar = (char) => {
+  return char === '*';
+}
+
+const getGearsCoordinates = (matrix) => {
+  const gearsCoordinates = [];
+  matrix.forEach((row, rowIndex) => {
+    row.forEach((char, charIndex) => {
+      if (isStar(char)) {
+        gearsCoordinates.push([rowIndex, charIndex]);
+      }
+    })
+  })
+  return gearsCoordinates;
+}
+
+const getGearsNumbersForRatio = (gearCoordinates, numberCoordinatesWithNumbers) => {
+  const numbersAdjacentToGears = [];
+  gearCoordinates.forEach((gearCoordinate) => {
+    const gearAdjacentCoordinates = getEveryAdjacentCoordinates(gearCoordinate);
+    const numbersAdjacentToGear = [];
+    numberCoordinatesWithNumbers.forEach((numberCoordinatesWithNumber) => {
+      const [numberCoordinates] = numberCoordinatesWithNumber;
+      gearAdjacentCoordinates.forEach((gearAdjacentCoordinate) => {
+        const [gearAdjacentRow, gearAdjacentColumn] = gearAdjacentCoordinate;
+        numberCoordinates.forEach((numberCoordinate) => {
+          const [numberRow, numberColumn] = numberCoordinate;
+          if (gearAdjacentRow === numberRow && gearAdjacentColumn === numberColumn) {
+            if (!numbersAdjacentToGear.includes(numberCoordinatesWithNumber)) {
+              numbersAdjacentToGear.push(numberCoordinatesWithNumber);
+            }
+          }
+        })
+      })
+    })
+    if (numbersAdjacentToGear.length === 2) {
+      numbersAdjacentToGears.push(numbersAdjacentToGear);
+    }
+  })
+  return numbersAdjacentToGears;
+}
+
+const gearsCoordinates = getGearsCoordinates(matrix);
+
+const gearsNumbersForRatio = getGearsNumbersForRatio(
+  gearsCoordinates,
+  everyNumberCoordinatesWithNumbers
+)
+
+const gearsRatios = gearsNumbersForRatio.map((gearNumbersForRatio) => {
+  return gearNumbersForRatio[0][1] * gearNumbersForRatio[1][1];
+})
+
+const sumGearsRatios = gearsRatios.reduce((acc, gearRatio) => {
+  return acc + gearRatio;
+}, 0);
+
+fs.writeFileSync('./Day 3/output2.txt', sumGearsRatios.toString(), 'utf8');
+
 module.exports = { isSpecialChar, getEveryAdjacentCoordinates };
