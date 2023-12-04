aa7c4a6f1a7a3be73679da79b7db2059affeb140
Paul <prenoult@me.com>
2023-12-04 22:48:24 +0100

    feat: Add Day4 part2

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const input = fs.readFileSync('./Day 4/input.txt', 'utf8').split('\n');
+const input_test = fs.readFileSync('./Day 4/input_test.txt', 'utf8').split('\n');
 input.pop();
 
 const removeLabel = (row) => {
@@ -41,4 +42,44 @@ const res = input.reduce((acc, row) => {
 
 fs.writeFileSync('./Day 4/output.txt', res.toString());
 
-module.exports = { getPointsFromRow };
+// part #2
+const getMatchingNumbersFromRow = (row) => {
+  const winningNumbers = getNumbersFromString(splitNumbers(removeLabel(row))[0]);
+  const numbersIHave = getNumbersFromString(splitNumbers(removeLabel(row))[1]);
+  return numbersIHave.reduce((acc, number) => {
+    if (winningNumbers.includes(number)) {
+      return acc + 1
+    }
+    return acc
+  }, 0);
+}
+
+const getWonScratchCards = (row, index) => {
+  const pointsFromRow = getMatchingNumbersFromRow(row)
+  const arr = [];
+  for (let i = 0; i < pointsFromRow; i++) {
+    arr.push(index + 1 + i);
+  }
+  return arr
+}
+
+
+const createInputWithCopiedCards = (input) => {
+  const inputWhichWillMutate = [...input];
+  for (let i = 0; i < inputWhichWillMutate.length; i++) {
+    const row = inputWhichWillMutate[i];
+    const realIndex = input.findIndex((element) => element === row)
+    const wonScratchcards = getWonScratchCards(row, realIndex);
+    if (wonScratchcards.length > 0) {
+      wonScratchcards.forEach((card) => {
+        inputWhichWillMutate.push(input[card])
+      })
+    }
+  }
+  return inputWhichWillMutate
+}
+
+const res2 = createInputWithCopiedCards(input).length
+fs.writeFileSync('./Day 4/output2.txt', res2.toString());
+
+module.exports = { getPointsFromRow, getWonScratchCards, createInputWithCopiedCards };
--- Day 4/index.test.js
+++ Day 4/index.test.js
@@ -1,25 +1,55 @@
-const { getPointsFromRow } = require('./index');
+const { getPointsFromRow, getWonScratchCards, createInputWithCopiedCards } = require('./index');
+const fs = require('fs');
+const input = fs.readFileSync('./Day 4/input_test.txt', 'utf8').split('\n');
 
-test('Card 1 from example', () => {
+test('Card 1 from example #part1', () => {
   expect(getPointsFromRow("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")).toBe(8);
 });
 
-test('Card 2 from example', () => {
+test('Card 2 from example #part1', () => {
   expect(getPointsFromRow("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")).toBe(2);
 });
 
-test('Card 3 from example', () => {
+test('Card 3 from example #part1', () => {
   expect(getPointsFromRow("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")).toBe(2);
 });
 
-test('Card 4 from example', () => {
+test('Card 4 from example #part1', () => {
   expect(getPointsFromRow("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")).toBe(1);
 });
 
-test('Card 5 from example', () => {
+test('Card 5 from example #part1', () => {
   expect(getPointsFromRow("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")).toBe(0);
 });
 
-test('Card 6 from example', () => {
+test('Card 6 from example #part1', () => {
   expect(getPointsFromRow("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")).toBe(0);
+});
+
+test('Card 1 from example #part2', () => {
+  expect(getWonScratchCards("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 0)).toStrictEqual([1, 2, 3, 4]);
+});
+
+test('Card 2 from example #part2', () => {
+  expect(getWonScratchCards("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", 1)).toStrictEqual([2, 3]);
+});
+
+test('Card 3 from example #part2', () => {
+  expect(getWonScratchCards("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2)).toStrictEqual([3, 4]);
+});
+
+test('Card 4 from example #part2', () => {
+  expect(getWonScratchCards("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", 3)).toStrictEqual([4]);
+});
+
+test('Card 5 from example #part2', () => {
+  expect(getWonScratchCards("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", 4)).toStrictEqual([]);
+});
+
+test('Card 6 from example #part2', () => {
+  expect(getWonScratchCards("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11", 5)).toStrictEqual([]);
+});
+
+test('Result from example #part2', () => {
+  expect(createInputWithCopiedCards(input).length).toBe(30);
 });
