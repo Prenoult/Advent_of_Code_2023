ae85a277f764667b9fcf0039fd4658b1968dfd95
Paul <prenoult@me.com>
2023-12-09 19:38:02 +0100

    feat: Add Day9 part1

---
+++ Day 9/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { get } = require('http');
+const input = fs.readFileSync('./Day 9/input.txt', 'utf-8').split('\n');
+input.pop();
+
+const sequences = input.map(row => row.split(' '));
+const firstSequence = sequences[0];
+
+const getNextSequence = (sequence) => {
+  const nextSequence = [];
+  sequence.forEach((number, index) => {
+    if (index === sequence.length - 1) {
+      return;
+    }
+    const currentNumber = parseInt(number);
+    const nextNumber = parseInt(sequence[index + 1]);
+    const difference = nextNumber - currentNumber;
+    nextSequence.push(difference);
+  });
+  return nextSequence;
+}
+
+const calculateNextTerm = (sequence) => {
+  let nextSeq = getNextSequence(sequence);
+  const res = [sequence]
+  while (!nextSeq.every(number => number === 0)) {
+    res.push(nextSeq);
+    nextSeq = getNextSequence(nextSeq);
+  }
+  res.push(nextSeq)
+  res.reverse();
+  return res.reduce((acc, curr) => parseInt(acc) + parseInt(curr[curr.length - 1]), 0)
+}
+
+const input_test = fs.readFileSync('./Day 9/input_test.txt', 'utf-8').split('\n');
+const sequences_test = input_test.map(row => row.split(' '));
+const res_test = sequences_test.reduce(
+  (acc, sequence) => parseInt(acc) + calculateNextTerm(sequence),
+  0
+);
+
+const res = sequences.reduce((acc, sequence) => parseInt(acc) + calculateNextTerm(sequence), 0);
+fs.writeFileSync('./Day 9/output.txt', res.toString());
