f0e5df0564e5740c3fb145c029aeb0c7984003a3
Paul <prenoult@me.com>
2023-12-09 15:06:20 +0100

    chore: Add Day8 part2 WIP

--- Day 8/index.js
+++ Day 8/index.js
@@ -31,7 +31,6 @@ const getNbOfSteps = (instructions, nodes) => {
   let index = 0;
   let nbOfSteps = 0;
   while (currentNode[0] !== 'ZZZ') {
-    console.log(currentNode[0])
     const nextNode = getNextNode(currentNode, instructions[index]);
     currentNode = nodes.find((node) => node[0] === nextNode);
     nbOfSteps++;
@@ -50,3 +49,50 @@ const nodes_test2 = getNodes(input_test2);
 
 const res = getNbOfSteps(instructions, nodes);
 fs.writeFileSync('./Day 8/output.txt', res.toString());
+
+// part #2
+const getStartingNodes = (nodes) => {
+  return nodes.filter((node) => {
+    const letters = node[0].split('');
+    return letters[2] === 'A';
+  });
+}
+
+const isEndingNode = (node) => {
+  const letters = node[0].split('');
+  return letters[2] === 'Z';
+}
+
+const isEveryNodesAnEndingNode = (nodes) => {
+  return nodes.every((node) => isEndingNode(node));
+}
+
+const getNbOfSteps2 = (instructions, nodes) => {
+  const startingNodes = getStartingNodes(nodes);
+  let currentNodes = startingNodes;
+  let index = 0;
+  let nbOfSteps = 0;
+  while (!isEveryNodesAnEndingNode(currentNodes)) {
+    const nextNodes = currentNodes.map((node) => {
+      const nextNode = getNextNode(node, instructions[index]);
+      return nodes.find((node) => node[0] === nextNode);
+    });
+    console.log("instructions[index] : ", instructions[index])
+    console.log("nextNodes : ", nextNodes);
+    currentNodes = nextNodes;
+    nbOfSteps++;
+    index = (index + 1) % instructions.length;
+  }
+  console.log("currentNodes : ", currentNodes)
+  console.log("nbOfSteps : ", nbOfSteps)
+  return nbOfSteps;
+}
+
+const input_test_part2 = fs.readFileSync('./Day 8/input_test_part2.txt', { encoding: 'utf-8' }).split('\n');
+const nodes_test_part2 = getNodes(input_test_part2);
+const instructions_test_part2 = getInstruction(input_test_part2);
+// console.log(getNbOfSteps2(instructions_test_part2, nodes_test_part2));
+// console.log(getStartingNodes(nodes));
+// console.log(getNbOfSteps2(instructions, nodes));
+const res2 = getNbOfSteps2(instructions, nodes);
+fs.writeFileSync('./Day 8/output.txt', res2.toString());
